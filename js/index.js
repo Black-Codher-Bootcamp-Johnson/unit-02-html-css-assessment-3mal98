06deae717b18fcfde6b28ec78740c589ed20affb
3mal98 <amalahmed_3@hotmail.com>
2022-04-15 20:28:10 +0100

    Add scroll to top button

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const toTop = document.querySelector('.to-top');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -8,5 +9,12 @@ navToggle.addEventListener('click', () => {
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
+    })
+})
+
+toTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
     })
 })
